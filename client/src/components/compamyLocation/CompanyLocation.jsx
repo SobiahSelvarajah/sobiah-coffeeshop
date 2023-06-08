51717a7f39c6912fd3ddd53dc5ba32df04d847c2
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-06-08 20:32:57 +0100

    components: companyLocation added

---
+++ client/src/components/compamyLocation/CompanyLocation.jsx
@@ -0,0 +1,14 @@
+import { HiLocationMarker } from "react-icons/hi"
+
+const CompanyLocation = () => {
+  return (
+        <div className="companyLocation__container">
+        <HiLocationMarker />
+        <h5 className="companyLocation__number">
+            26 Lover's Lane, London
+        </h5>
+    </div>
+  )
+}
+
+export default CompanyLocation
--- client/src/components/companyDetails/CompanyDetails.jsx
+++ client/src/components/companyDetails/CompanyDetails.jsx
@@ -1,12 +1,14 @@
 import FooterLogo from '../footerLogo/FooterLogo'
 import CompanyNumber from '../companyNumber/CompanyNumber'
 import './CompanyDetails.scss'
+import CompanyLocation from '../compamyLocation/CompanyLocation'
 
 const CompanyDetails = () => {
   return (
     <div className="companyDetails__container">
         <FooterLogo />
         <CompanyNumber />
+        <CompanyLocation />
     </div>
   )
 }
