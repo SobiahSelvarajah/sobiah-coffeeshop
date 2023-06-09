47d76ed1b6b1ec6a160d1f4732294423834797e8
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-06-09 20:01:20 +0100

    components: companyName added

--- client/src/components/companyDetails/CompanyDetails.jsx
+++ client/src/components/companyDetails/CompanyDetails.jsx
@@ -1,13 +1,15 @@
 import FooterLogo from '../footerLogo/FooterLogo'
+import CompanyName from '../companyName/CompanyName'
 import CompanyNumber from '../companyNumber/CompanyNumber'
-import './CompanyDetails.scss'
 import CompanyLocation from '../compamyLocation/CompanyLocation'
 import CompanyEmail from '../companyEmail/CompanyEmail'
+import './CompanyDetails.scss'
 
 const CompanyDetails = () => {
   return (
     <div className="companyDetails__container">
         <FooterLogo />
+        <CompanyName />
         <CompanyNumber />
         <CompanyLocation />
         <CompanyEmail />
---
+++ client/src/components/companyName/CompanyName.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CompanyName = () => {
+  return (
+    <h3 className="companyName__name">
+        My Little Valentine
+    </h3>
+  )
+}
+
+export default CompanyName
